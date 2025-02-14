e1bfefe27a63c124a651bdcd8441356dc2f8c19e
Arpitkandwal <arpit.kandwal2004@gmail.com>
2025-02-14 13:45:07 +0530

    new

--- config/server.js
+++ config/server.js
@@ -2,10 +2,11 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    // keys: env.array('APP_KEYS'),
+    keys: ['key-2', 'key-1'],
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
-  keys: ['key-2', 'key-1'],
+  
 });
