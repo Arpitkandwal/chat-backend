7c714812a9da9b63c0afb18a373231b6deecfd93
Arpitkandwal <arpit.kandwal2004@gmail.com>
2025-02-14 14:22:31 +0530

    d

--- config/plugins.js
+++ config/plugins.js
@@ -1,10 +1,9 @@
 module.exports = {
-    // other plugin configurations...
     'users-permissions': {
       enabled: true,
       config: {
         jwt: {
-          jwtSecret: process.env.JWT_SECRET || 'axjnshh12321', // You can use an environment variable here
+          jwtSecret: 'my-hardcoded-secret-key',
         },
       },
     },
