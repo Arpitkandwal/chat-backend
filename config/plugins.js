a957b865faf2bd610bb3b217a676f2b1e7426b76
Arpitkandwal <arpit.kandwal2004@gmail.com>
2025-02-14 14:09:29 +0530

    n

--- config/plugins.js
+++ config/plugins.js
@@ -1,13 +1,14 @@
     module.exports = {
         // other plugin configurations...
-        'users-permissions': {
-        enabled: true,
-        config: {
-            jwt: {
-            // @ts-ignore
-            jwtSecret: crypto.randomBytes(16).toString('base64'), // Replace this with your generated secret
-            },
-        },
-        },
+        jwtSecret: "axjnshh12321"
+        // 'users-permissions': {
+        // enabled: true,
+        // config: {
+        //     jwt: {
+        //     // @ts-ignore
+        //     jwtSecret: crypto.randomBytes(16).toString('base64'), // Replace this with your generated secret
+        //     },
+        // },
+        // },
     };
     
