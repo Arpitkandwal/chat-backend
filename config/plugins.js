b77fea93502c070edfd936bf5eff023324a112d9
Arpitkandwal <arpit.kandwal2004@gmail.com>
2025-02-14 13:51:03 +0530

    main

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,12 @@
-module.exports = () => ({});
+module.exports = {
+    // other plugin configurations...
+    'users-permissions': {
+      enabled: true,
+      config: {
+        jwt: {
+          jwtSecret: 'aDdlHfGo1MO8zkJXeKKq3g==', // Replace this with your generated secret
+        },
+      },
+    },
+  };
+  
